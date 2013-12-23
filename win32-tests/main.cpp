5107ee096af1b5fc195318fcafd48de79a1b28a6
Sergejs Belajevs <sergejs.belajevs@gmail.com>
2013-12-23 23:24:18 +0200

    Rewrote window creation code in OOP fashion

--- win32-tests/main.cpp
+++ win32-tests/main.cpp
@@ -3,15 +3,6 @@
 
 static const char WND_CLASS_NAME[] = "win32-tests";
 
-struct WindowContext
-{
-    ATOM classAtom;
-    HINSTANCE instance;
-    HWND window;
-    HDC dc;
-    HGLRC context;
-};
-
 static LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
@@ -27,30 +18,15 @@ static LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
     return DefWindowProc(hwnd, msg, wParam, lParam );
 }
 
-static ATOM registerWindowClass(HINSTANCE instance)
+static int getPixelFormatId(const HDC& dc)
 {
-    WNDCLASS wc;
-    ZeroMemory(&wc, sizeof(wc));
-
-    wc.style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-    wc.lpfnWndProc   = (WNDPROC) wndProc;
-    wc.hInstance     = instance;
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hIcon         = LoadIcon(NULL, IDI_WINLOGO);
-    wc.lpszClassName = WND_CLASS_NAME;
-
-    return RegisterClass( &wc );
-}
-
-static int getPixelFormatId(const WindowContext& wctx)
-{
-    int count = DescribePixelFormat(wctx.dc, 1, sizeof(PIXELFORMATDESCRIPTOR), NULL);
+    int count = DescribePixelFormat(dc, 1, sizeof(PIXELFORMATDESCRIPTOR), NULL);
 
     for (int i=1; i<=count; i++)
     {
         // TODO: choose the closest from all if perfect match is not found
         PIXELFORMATDESCRIPTOR pfd;
-        DescribePixelFormat(wctx.dc, i, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
+        DescribePixelFormat(dc, i, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
 
         static const DWORD REQUIRED_FLAGS = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
         if ((pfd.dwFlags & REQUIRED_FLAGS) != REQUIRED_FLAGS)
@@ -69,7 +45,7 @@ static int getPixelFormatId(const WindowContext& wctx)
         }
 
         if (pfd.cRedBits == 8 && pfd.cGreenBits == 8 && pfd.cBlueBits == 8 && pfd.cAlphaBits == 8
-            && pfd.cDepthBits == 24 && pfd.cStencilBits == 0)
+            && pfd.cDepthBits == 24 && pfd.cStencilBits == 8)
         {
             return i;
         }
@@ -78,117 +54,152 @@ static int getPixelFormatId(const WindowContext& wctx)
     return 0;
 }
 
-static void createContext(WindowContext& wctx, int pixelFormatId)
+class WindowContext
 {
-    PIXELFORMATDESCRIPTOR pfd;
-    SetPixelFormat(wctx.dc, pixelFormatId, &pfd);
-    wctx.context = wglCreateContext(wctx.dc);
-}
-
-static bool openWindow(WindowContext& wctx, int x, int y, int w, int h)
-{
-    DWORD wndStyle = WS_CLIPSIBLINGS | WS_CLIPCHILDREN 
-        | WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU 
-        | WS_MINIMIZEBOX |  WS_MAXIMIZEBOX | WS_SIZEBOX;
-    DWORD wndExStyle = WS_EX_APPWINDOW;
-
-    int fullW = w;
-    int fullH = h;
-
-    wctx.window = CreateWindowEx(
-        wndExStyle, WND_CLASS_NAME, "My window", wndStyle,
-        x, y, fullW, fullH,
-        NULL, NULL, wctx.instance, NULL
-    );
-
-    wctx.dc = GetDC(wctx.window);
-    createContext(wctx, getPixelFormatId(wctx));
-    wglMakeCurrent(wctx.dc, wctx.context);
-
-    ShowWindow(wctx.window, SW_SHOWNORMAL);
-    SetFocus(wctx.window);
-
-    return true;
-}
-
-static void closeWindow(WindowContext& wctx)
-{
-    wglMakeCurrent( NULL, NULL );
-    wglDeleteContext(wctx.context);
-    wctx.context = NULL;
-
-    ReleaseDC(wctx.window, wctx.dc );
-    wctx.dc = NULL;
-
-    DestroyWindow(wctx.window);
-    wctx.window = NULL;
-
-    UnregisterClass(WND_CLASS_NAME, wctx.instance);
-    wctx.classAtom = 0;
-}
-
-static void pollEvents(WindowContext& wctx)
-{
-    bool doClose = false;
-    MSG msg;
-
-    while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+public:
+    static WindowContext* open(int x, int y, int w, int h)
     {
-        switch (msg.message)
-        {
-            case WM_QUIT:
-                doClose = true;
-                break;
+        WindowContext* ctx = new WindowContext();
+        ctx->instance = GetModuleHandle(NULL);
 
-            default:
-                DispatchMessage(&msg);
-                break;
+        // Register a class first
+        {
+            WNDCLASS wc;
+            ZeroMemory(&wc, sizeof(wc));
+
+            wc.style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+            wc.lpfnWndProc   = (WNDPROC) wndProc;
+            wc.hInstance     = ctx->instance;
+            wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+            wc.hIcon         = LoadIcon(NULL, IDI_WINLOGO);
+            wc.lpszClassName = WND_CLASS_NAME;
+
+            ctx->classAtom = RegisterClass(&wc);
+        }
+
+        // Now we're ready to open the window
+        {
+            DWORD wndStyle = WS_CLIPSIBLINGS | WS_CLIPCHILDREN 
+                | WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU 
+                | WS_MINIMIZEBOX |  WS_MAXIMIZEBOX | WS_SIZEBOX;
+            DWORD wndExStyle = WS_EX_APPWINDOW;
+
+            int fullW = w;
+            int fullH = h;
+
+            ctx->window = CreateWindowEx(
+               wndExStyle, WND_CLASS_NAME, "My window", wndStyle,
+               x, y, fullW, fullH,
+               NULL, NULL, ctx->instance, NULL
+            );
+        }
+
+        // Create OpenGL context
+        {
+            ctx->dc = GetDC(ctx->window);
+            PIXELFORMATDESCRIPTOR pfd;
+            int bestPixelFormatId = getPixelFormatId(ctx->dc);
+            SetPixelFormat(ctx->dc, bestPixelFormatId, &pfd);
+            ctx->context = wglCreateContext(ctx->dc);
+            wglMakeCurrent(ctx->dc, ctx->context);
+        }
+
+        // And finally, make it visible
+        ShowWindow(ctx->window, SW_SHOWNORMAL);
+        SetFocus(ctx->window);
+
+        return ctx;
+    }
+
+    void run()
+    {
+        float rd = 0.001f;
+        float gd = 0.005f;
+        float bd = 0.0025f;
+
+        float r = 0;
+        float g = 0;
+        float b = 0;
+
+        while (doFinish == false) 
+        {
+            if (r > 1.0f || r < 0.f) rd = -rd;
+            if (g > 1.0f || g < 0.f) gd = -gd;
+            if (b > 1.0f || b < 0.f) bd = -bd;
+            r += rd;
+            g += gd;
+            b += bd;
+
+            Sleep(15);
+            swapBuffers();
+            poll();
+
+            glClearColor(r, g, b, 1.f);
+            glClear(GL_COLOR_BUFFER_BIT);
         }
     }
 
-    if (doClose)
+    ~WindowContext()
     {
-        closeWindow(wctx);
-    }
-}
+        wglMakeCurrent(NULL, NULL);
+        wglDeleteContext(context);
+        context = NULL;
 
-static void swapBuffers(WindowContext& wctx)
-{
-    SwapBuffers(wctx.dc);
-}
+        ReleaseDC(window, dc);
+        dc = NULL;
+
+        DestroyWindow(window);
+        window = NULL;
+
+        UnregisterClass(WND_CLASS_NAME, instance);
+        classAtom = 0;
+    }
+
+private:
+    WindowContext(const WindowContext& ctx);
+    WindowContext& operator = (const WindowContext& other);
+
+    WindowContext(): doFinish(false)
+    {
+    }
+
+    void poll()
+    {
+        MSG msg;
+
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            switch (msg.message)
+            {
+                case WM_QUIT:
+                    doFinish = true;
+                    break;
+
+                default:
+                    DispatchMessage(&msg);
+                    break;
+            }
+        }
+    }
+
+    void swapBuffers()
+    {
+        SwapBuffers(dc);
+    }
+
+    bool doFinish;
+    ATOM classAtom;
+    HINSTANCE instance;
+    HWND window;
+    HDC dc;
+    HGLRC context;
+};
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-    WindowContext wctx;
-
-    wctx.instance = GetModuleHandle(NULL);
-    wctx.classAtom = registerWindowClass(wctx.instance);
-    openWindow(wctx, 100, 100, 640, 480);
-
-    float rd = 0.001f;
-    float gd = 0.005f;
-    float bd = 0.0025f;
-
-    float r = 0;
-    float g = 0;
-    float b = 0;
-
-    while (wctx.window) 
-    {
-        if (r > 1.0f || r < 0.f) rd = -rd;
-        if (g > 1.0f || g < 0.f) gd = -gd;
-        if (b > 1.0f || b < 0.f) bd = -bd;
-        r += rd;
-        g += gd;
-        b += bd;
-
-        Sleep(15);
-        swapBuffers(wctx);
-        pollEvents(wctx);
-
-        glClearColor(r, g, b, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);
-    }
+    WindowContext* window = WindowContext::open(100, 100, 640, 480);
+    window->run();
+    delete window;
 
     return 0;
 }
